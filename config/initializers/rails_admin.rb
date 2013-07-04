a5d80351b1bc1e7a61d2a39d7f859082d36caff6
Cristobal Lama <cristoballama@gmail.com>
2013-07-04 14:07:53 -0400

    Agregado links entre app principal y admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,6 +1,6 @@
 RailsAdmin.config do |config|
 
   config.current_user_method { current_user } #auto-generated
-  config.included_models = ["Recoleccion","DataFile","User"]
+  config.included_models = ["Recoleccion","DataFile"]
   config.authorize_with :cancan
 end
